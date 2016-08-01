e00b3a5d0e541508ace44e64be9ff76ff62299de
Misha <mkaravaev@gmail.com>
2016-08-01 14:09:10 +0500

    add geoNear to Task

--- lib/models/task.rb
+++ lib/models/task.rb
@@ -7,13 +7,15 @@ class Task
   field :state, type: Symbol, default: :new
   field :pickup, type: Array
   field :delivery, type: Array
-  field :executor, type: String
+
+  index({pickup: "2d"})
+  index({delivery: "2d"})
 
   validates :state, inclusion: { in: STATES }
+  validates_presence_of :initiator
 
-  has_one :owner, ->{ where(type: :manager) }, class_name: "User"
-  has_one :executor, ->{ where(type: :manager) }, class_name: "User"
-
+  belongs_to :creator, class_name: "User"
+  belongs_to :executor, class_name: "User"
 
   def assign!
     self.update({state: :assigned!})
@@ -23,7 +25,7 @@ class Task
     self.update({state: :done})
   end
 
-  def self.nearest_for(lat, lng)
+  def self.nearest(lat, lng)
     pickup.geo_near([lat, lng])
   end
 end
